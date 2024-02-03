8b7825b2e0f20e02ae15f5f987151b438ff3efea
Efrun Evdi <efruncetkin@gmail.com>
2024-02-03 23:06:01 +0300

    8. Ders Sonu

---
+++ UdemyEFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyEFCore.DatabaseFirst.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=EFRUN\\SQLEXPRESS; Initial Catalog=UdemyEFCoreDatabaseFirstDb; Integrated Security=True; Trust Server Certificate=False;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- UdemyEFCore.DatabaseFirst/Program.cs
+++ UdemyEFCore.DatabaseFirst/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyEFCore.DatabaseFirst.DAL;
+
+using (var _context = new AppDbContext())
+{
+    var product = await _context.Products.ToListAsync();
+
+    product.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name}");
+    });
+
+} // Async olduğu için await ekledik. Async kullanıyoruz çünkü using ifadesinden çıktığı anda context nesnesi memoryden çıkacak.
